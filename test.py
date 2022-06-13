1c3549be8ad9cebde19c9b64a65289882486aada
EntrishKanter <94009238+EntrishKanter@users.noreply.github.com>
2022-06-13 21:46:26 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-connect = sqlite3.connect('data.db')
-cursor = connect.cursor()
-
-
-connect.execute("INSERT INTO 'Тест опрос' VALUES(1, 'Опрос', '[{'text': 'hi', 'callback': '5'}]')")
-connect.commit()
